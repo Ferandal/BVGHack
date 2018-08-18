7923faf0476d1a3a0987ff13666536e8941baed4
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2018-08-18 23:35:44 +0200

    works now

--- serverToArduino/colorSetting.js
+++ serverToArduino/colorSetting.js
@@ -1,7 +1,17 @@
 'use strict'
 
+var execFile = require('child_process').execFile
+var program = "build/Release/serverToArduino";
+// execFile will return immediately.
+var child = execFile(program, [],
+  function (error, stdout, stderr) {
+    //executed when finished
+});
+
+
 function setColor(wagon, status) {
-  return wagon+","+status;
+    child.stdin.setEncoding('utf-8');
+    child.stdin.write(wagon+","+status + "\n");
 }
 
 module.exports = setColor;
--- serverToArduino/main.cpp
+++ serverToArduino/main.cpp
@@ -8,31 +8,29 @@
 using namespace std;
 
 char *port_name = "COM3";
-SerialPort arduino(port_name);
 
 char incomingData[MAX_DATA_LENGTH];
 
-void sendColorSettingsToArduino(string colorsetting) {
-	if (arduino.isConnected()) {
+void sendColorSettingsToArduino(string colorsetting, SerialPort* arduino) {
 		char *c_string = new char[colorsetting.size() + 1];
 		copy(colorsetting.begin(), colorsetting.end(), c_string);
 
 		c_string[colorsetting.size()] = '\n';
 
-		arduino.writeSerialPort(c_string, MAX_DATA_LENGTH);
+		arduino->writeSerialPort(c_string, MAX_DATA_LENGTH);
 
 		delete[] c_string;
-	}
 }
 
 int main()
 {
+	SerialPort arduino(port_name);
 	if (arduino.isConnected()) cout << "Connection Established" << endl;
 	else cout << "ERROR, check port name";
 
 	while (arduino.isConnected()) {
 		string input_string;
 		getline(cin, input_string);
-		sendColorSettingsToArduino(input_string);
+		sendColorSettingsToArduino(input_string, &arduino);
 	}
 }

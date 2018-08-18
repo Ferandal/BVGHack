aba44180c1383661c470988d01809d466736a263
leogeier <leonard.geier@hotmail.com>
2018-08-18 23:36:25 +0200

    Construct and send json on request

--- pressureSensor/connections_template.h
+++ pressureSensor/connections_template.h
@@ -4,7 +4,6 @@
 const char* ssid = "NETWORK SSID HERE";
 const char* password = "NETWORK PASSWORD HERE";
 
-const char* demoSender = "DEMO SERVER IP HERE"
-const char* centralServer = "CENTRAL SERVER IP HERE"
+const char* seatingUrl = "SEATING URL HERE"
 
 #endif
--- pressureSensor/pressureSensor.ino
+++ pressureSensor/pressureSensor.ino
@@ -1,11 +1,21 @@
+// In order to signal that the train switched stations, send a POST request to hostname/nextStation
+// E.g.: curl -X POST http://123.45.67.89/nextStation
+
 #include "connections.h"
 
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-
 #define PERSON_WEIGHT 150
 #define WEIGHT_PIN A0
+
+#define MODEL "D"
+#define WAGON_COUNT 6
+#define SEAT_COUNT 36
+
+int currentStation = 0;
+#define MAX_STATION 28
+int travelDirection = 1;
 
 HTTPClient http;
 WiFiServer server(80);
@@ -51,22 +61,77 @@ void loop() {
  
   // Read the first line of the request
   String request = client.readStringUntil('\r');
-  Serial.println(request);
   client.flush();
 
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println(""); //  do not forget this one
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>Done</html>");
+  // Handle request
+  if (request.indexOf("/nextStation") != -1) {
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/plain");
+    client.println("");
+    client.println("Received");
 
-  
+    sendSeatingJson();
+  }
+  else {
+    client.println("HTTP/1.1 404 Not Found");
+  }
+}
+
+bool isPersonSitting(int carIndex, int seatIndex) {
+  if (carIndex != 0 || seatIndex != 0) return true;
+  int value = analogRead(WEIGHT_PIN);
+  if (value >= PERSON_WEIGHT) return true;
+  return false;
+}
+
+int getCurrentStation() {
+  int station = currentStation;
+  currentStation += travelDirection;
+  if (currentStation == 0) {
+    travelDirection = 1;
+  }
+  else if (currentStation == MAX_STATION) {
+    travelDirection = -1;
+  }
+  return station;
+}
+
+String getDirectionString() {
+  return travelDirection == 1 ? "H" : "R";
+}
+
+int getWagonSeatingCount(int wagonIndex) {
+  int count = 0;
+  for (int i=0; i < SEAT_COUNT; i++) {
+    if (isPersonSitting(wagonIndex, i)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+String makeSeatingJson() {
+  String jsonString = "{\"line\":\"U6\",\"id\":16,\"baureihe\":\"D\"";
+  jsonString += ",\"station\":" + String(getCurrentStation());
+  jsonString += ",\"direction\":\"" + getDirectionString() + "\"";
+  jsonString += ",\"wagons\":[";
+  for (int i=0; i < WAGON_COUNT; i++) {
+    jsonString += String(getWagonSeatingCount(i));
+    if(i < WAGON_COUNT - 1) {
+      jsonString += ",";
+    }
+  }
+  jsonString += "]}";
+  return jsonString;
+}
+
+void sendSeatingJson() {
   HTTPClient http;  //Declare an object of class HTTPClient
-
-  http.begin("http://192.168.193.144");  //Specify request destination
-  int httpCode = http.GET();               
+  http.begin(seatingUrl);  //Specify request destination
+  http.addHeader("Content-Type", "application/json");
+  String jsonString = makeSeatingJson();
+  int httpCode = http.POST(jsonString.c_str());            
   http.end();   //Close connection
   Serial.println(httpCode);
- 
 }
 
